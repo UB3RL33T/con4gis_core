3e049464cebabc30c3fba743b51ab24e70dc4bce
coastforge-tdo <tobias.dobbrunz@kuestenschmiede.de>
2015-01-06 12:37:34 +0100

    Change Copyright-year

--- config/config.php
+++ config/config.php
@@ -7,7 +7,7 @@
      * @package   con4gis
      * @author    Tobias Dobbrunz &#40;http://www.kuestenschmiede.de&#41;
      * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
-     * @copyright Küstenschmiede GmbH Software & Design 2014
+     * @copyright Küstenschmiede GmbH Software & Design 2014 - 2015
      * @link      https://www.kuestenschmiede.de
      * @filesource
      */
