27730e2df0a41d940fae722c1a15d5f61bfbd96e
coastforge-tdo <tobias.dobbrunz@kuestenschmiede.de>
2015-10-01 10:02:11 +0200

    Change versionnumber

--- config/config.php
+++ config/config.php
@@ -14,7 +14,7 @@
 
 
 $GLOBALS['con4gis_core_extension']['installed'] = true;
-$GLOBALS['con4gis_core_extension']['version']   = '1.1.2';
+$GLOBALS['con4gis_core_extension']['version']   = '1.2.0';
 
 // API-Registration
 $GLOBALS['TL_API'] = array();
