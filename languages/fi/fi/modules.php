6844bd053359b211650dd0eb0871bc82488072a9
coastforge-mei <info@kuestenschmiede.de>
2016-05-03 14:41:02 +0200

    con4gis-Core Go Finland

---
+++ languages/fi/fi/modules.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * @version   php 5
+ * @package   con4gis
+ * @author    Joona Ilomäki
+ * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
+ * @copyright Küstenschmiede GmbH Software & Design 2014 - 2016
+ * @link      https://www.kuestenschmiede.de
+ * @filesource
+ */
+
+
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['con4gis'] 				= array( 'con4gis (Rakennussarja)', 'www.con4gis.org' );
+$GLOBALS['TL_LANG']['MOD']['c4g_core'] 				= array( 'Informaatio & apu', 'Informaatio ja apu con4gis:seen.' );
+
+/**
+ * Content elements
+ */
+$GLOBALS['TL_LANG']['CTE']['c4g_activationpage']          = array('Aktivointisivu (con4gis)', 'Tällä moduulilla sinä sisällytät aktivointisivun.');
+?>
