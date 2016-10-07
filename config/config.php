c4d7e89906593a1874296f86c7630c20d53b213e
coastforge-mei <info@kuestenschmiede.de>
2016-10-07 15:22:49 +0200

    v3.4

--- config/config.php
+++ config/config.php
@@ -13,7 +13,7 @@
 
 $GLOBALS['con4gis_core_extension']['installed'] = true;
 $GLOBALS['con4gis_core_extension']['version']   = '1.6.8-snapshot';
-$GLOBALS['con4gis_core_extension']['con4gis_version'] = 'v3.3';
+$GLOBALS['con4gis_core_extension']['con4gis_version'] = 'v3.4';
 
 // API-Registration
 $GLOBALS['TL_API'] = array();
