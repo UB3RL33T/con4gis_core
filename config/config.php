6f4b8bbca15bd6f7a75f69e734bdbf8cd5dae4c8
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2017-03-23 11:06:40 +0100

    1.8.1-snapshot

--- config/config.php
+++ config/config.php
@@ -12,7 +12,7 @@
  */
 
 $GLOBALS['con4gis_core_extension']['installed'] = true;
-$GLOBALS['con4gis_core_extension']['version']   = '1.8.0';
+$GLOBALS['con4gis_core_extension']['version']   = '1.8.1-snapshot';
 $GLOBALS['con4gis_core_extension']['con4gis_version'] = 'v3.6';
 
 // API-Registration
