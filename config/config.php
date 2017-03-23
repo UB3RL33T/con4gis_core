7b19d731162f5c921c8a8f7dae0e1b3fdd33d5a7
Matthias Eilers <matthias.eilers@kuestenschmiede.de>
2017-03-23 10:46:18 +0100

    version prep

--- config/config.php
+++ config/config.php
@@ -12,8 +12,8 @@
  */
 
 $GLOBALS['con4gis_core_extension']['installed'] = true;
-$GLOBALS['con4gis_core_extension']['version']   = '1.7.2-snapshot';
-$GLOBALS['con4gis_core_extension']['con4gis_version'] = 'v3.5';
+$GLOBALS['con4gis_core_extension']['version']   = '1.8.0';
+$GLOBALS['con4gis_core_extension']['con4gis_version'] = 'v3.6';
 
 // API-Registration
 $GLOBALS['TL_API'] = array();
